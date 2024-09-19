6441873138be38a3148a362e9d707803a2e55d76
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2024-09-19 18:24:01 +0530

    main

---
+++ backend/model/usermodel.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        rewuired:true,
+        unique:true
+    },
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        lowercase: true,
+        unique: true,
+        match: [/\S+@\S+\.\S+/, "is invalid"]
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength: 6
+    },
+    Avatar:{
+        type:String,
+        default:""
+    },
+    refreshToken:{
+        type:String
+    },
+    Videos: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Video'
+        }
+    ]
+},
+{
+    timestamps:true
+}
+)
+ const User = new mongoose.model('user',userSchema);
