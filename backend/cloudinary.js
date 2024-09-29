b1aa376b2d4834d66c54f7e1789fe34e92452c4d
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2024-09-29 07:04:38 +0530

    main
    making cloudinary.js file and multer.js file for file uploading

---
+++ backend/cloudinary.js
@@ -0,0 +1,32 @@
+
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+try {
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API,
+        api_secret: process.env.CLOUD_SECRET
+    });
+} catch (error) {
+    console.error("Cloudinary configuration failed:", error);
+    throw new Error("Cloudinary configuration failed");
+}
+
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        fs.unlinkSync(localFilePath);
+        return response;
+    } catch (error) {
+        console.error("Cloudinary Upload Error:", error);
+        if (fs.existsSync(localFilePath)) {
+            fs.unlinkSync(localFilePath); 
+        }
+        throw new Error("Failed to upload to Cloudinary");
+    }
+};
+
+export { uploadCloudinary };
