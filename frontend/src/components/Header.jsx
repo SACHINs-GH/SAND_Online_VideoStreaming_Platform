d92eddfedd0d9aeff99f9ad9d5fbb549a4d9ea33
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2024-10-23 03:51:04 +0530

    main

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
   return (
     <div className="flex justify-between items-center p-4 bg-white shadow-md h-16">
-      <div className="flex items-center  p-3 w-1/6">
-        <h1 className="text-3xl">LOGO</h1>
+      <div className="flex items-center justify-center p-3 w-1/6">
+        <h1 className="text-xl">LOGO</h1>
       </div>
 
       <div className="flex items-center w-1/2">
@@ -18,8 +18,8 @@ function Header() {
         </button>
       </div>
 
-      <div className="flex items-center gap-3">
-        <h2 className="text-lg">User's Name</h2>
+      <div className="flex items-center gap-2">
+        <span className="hidden sm:inline text-sm">User's Name</span>
         <div className="h-10 w-10 rounded-full bg-gray-400 overflow-hidden">
           <img
             src=""
